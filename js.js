4773e82ce2fda155e160a49e0b606aaa4dc3f11a
Glib Lakeev <glakeev@gmail.com>
2022-07-06 10:48:33 -0400

    changed ui a bit added array for first var to calc (a);

--- js.js
+++ js.js
@@ -1,5 +1,10 @@
-//declare display #
+//declare globals
 let display = 0;
+let param;
+let aArray = new Array;
+let bArray = new Array;
+let a; 
+let b;
 
 //hook up display
 const showDisplay = document.querySelector("#result");
@@ -36,6 +41,7 @@ const operate = function (param, a, b){
             total = add(a,b);
             console.log("add total: "+total);
             return total;
+            break;
 
         case "subtractCase":
             total = subtract(a,b);
@@ -65,33 +71,49 @@ const operate = function (param, a, b){
 
 //oper button functionality
 const addButton = document.querySelector("#add");
-    addButton.addEventListener(`click`, function(e){
-        console.log("add button pressed");
-    })
+addButton.addEventListener(`click`, function(e){
+//    console.log("add button pressed");
+    param = "add";
+})
 
 const subtractButton = document.querySelector("#subtract");
 subtractButton.addEventListener(`click`, function(e){
-    console.log("subtract button pressed");
+    //console.log("subtract button pressed");
+    param = "subtract";
 })
 
 const multiplyButton = document.querySelector("#multiply");
 multiplyButton.addEventListener(`click`, function(e){
-    console.log("multiply button pressed");
+   // console.log("multiply button pressed");
+    param = "multiply";
 })
 
 const divideButton = document.querySelector("#divide");
 divideButton.addEventListener(`click`, function(e){
-    console.log("divide button pressed");
+    //console.log("divide button pressed");
+    param = "divide";
 })
 
 const equalButton = document.querySelector("#equal");
 equalButton.addEventListener(`click`, function(e){
-    console.log("equal button pressed");
+    //console.log("equal button pressed");
+    operate(param, a, b);
 })
 
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener(`click`, function (e){
-    console.log("clear button pressed");
+    //test 
+    //console.log("clear button pressed");
+    
+    //clear all values and variables
+    aArray = [];
+    bArray = [];
+    param;
+    a = 0;
+    b = 0;
+    display = 0;
+    showDisplay.innerHTML = display;
+
 })
 
 const percentButton = document.querySelector("#percent");
@@ -108,72 +130,325 @@ posnegButton.addEventListener(`click`, (e) => {
 
 const oneButton = document.querySelector("#one");
 oneButton.addEventListener(`click`, (e) => {
-    console.log("one button pressed");
-    display = 1;
+
+    if (param == null){
+
+        aArray.push(1);
+
+        console.log(aArray);
+
+        if (aArray.length > 5){
+            console.log("too long")
+            display = "ERR";
+            showDisplay.innerHTML = display;
+    
+        } else if (aArray.length <= 5) {
+
+            a = aArray.join("");
+            display = a;
+            showDisplay.innerHTML = display;
+
+        }
+
+
+
+    } else {
+
+        /* untested
+        bArray.push(1);
+        display = bArray.join("");
+        */
+    }
+
+    
+
     showDisplay.innerHTML = display;
 })
 
 const twoButton = document.querySelector("#two");
 twoButton.addEventListener(`click`, (e) => {
-    console.log("two button pressed");
-    display = 2;
-    showDisplay.innerHTML = display;
+
+    if (param == null){
+
+        aArray.push(2);
+        a = aArray.join("");
+
+        if (aArray.length > 5){
+            console.log("too long")
+            display = "ERR";
+            showDisplay.innerHTML = display;
+    
+        } else if(aArray.length <= 5) {
+
+            a = aArray.join("");
+            display = a;
+            showDisplay.innerHTML = display;
+
+        }
+
+
+    } else {
+
+        /* untested
+        bArray.push(2);
+        display = bArray.join("");
+        */
+    }
+
 })
 
 const threeButton = document.querySelector("#three");
 threeButton.addEventListener(`click`, (e) => {
-    console.log("three button pressed");
-    display = 3;
-    showDisplay.innerHTML = display;
+
+    if (param == null){
+
+        aArray.push(3);
+        a = aArray.join("");
+
+        if (aArray.length > 5){
+            console.log("too long")
+            display = "ERR";
+            showDisplay.innerHTML = display;
+    
+        } else if(aArray.length <= 5) {
+
+            a = aArray.join("");
+            display = a;
+            showDisplay.innerHTML = display;
+
+        }
+
+    } else {
+
+        /* untested
+        bArray.push(3);
+        display = bArray.join("");
+        */
+    }
+
 })
 
 const fourButton = document.querySelector("#four");
 fourButton.addEventListener(`click`, (e) => {
-    console.log("four button pressed");
-    display = 4;
-    showDisplay.innerHTML = display;
+
+    if (param == null){
+
+        aArray.push(4);
+        a = aArray.join("");
+
+        if (aArray.length > 5){
+            console.log("too long")
+            display = "ERR";
+            showDisplay.innerHTML = display;
+    
+        } else if(aArray.length <= 5) {
+
+            a = aArray.join("");
+            display = a;
+            showDisplay.innerHTML = display;
+
+        }
+
+    } else {
+
+        /* untested
+        bArray.push(4);
+        display = bArray.join("");
+        */
+    }
+
 })
 
 const fiveButton = document.querySelector("#five");
 fiveButton.addEventListener(`click`, (e) => {
-    console.log("five button pressed");
-    display = 5;
-    showDisplay.innerHTML = display;
+
+    if (param == null){
+
+        aArray.push(5);
+        a = aArray.join("");
+
+        if (aArray.length > 5){
+            console.log("too long")
+            display = "ERR";
+            showDisplay.innerHTML = display;
+    
+        } else if(aArray.length <= 5) {
+
+            a = aArray.join("");
+            display = a;
+            showDisplay.innerHTML = display;
+
+        }
+
+    } else {
+
+        /* untested
+        bArray.push(5);
+        display = bArray.join("");
+        */
+    }
+
 })
 
 const sixButton = document.querySelector("#six");
 sixButton.addEventListener(`click`, (e) => {
-    console.log("six button pressed");
-    display = 6;
-    showDisplay.innerHTML = display;
+
+    if (param == null){
+
+        aArray.push(6);
+        a = aArray.join("");
+
+        if (aArray.length > 5){
+            console.log("too long")
+            display = "ERR";
+            showDisplay.innerHTML = display;
+    
+        } else if(aArray.length <= 5) {
+
+            a = aArray.join("");
+            display = a;
+            showDisplay.innerHTML = display;
+
+        }
+
+    } else {
+
+        /* untested
+        bArray.push(6);
+        display = bArray.join("");
+        */
+    }
+
 })
 
 const sevenButton = document.querySelector("#seven");
 sevenButton.addEventListener(`click`, (e) => {
-    console.log("seven button pressed");
-    display = 7;
-    showDisplay.innerHTML = display;
+    if (param == null){
+
+        aArray.push(7);
+        a = aArray.join("");
+
+        if (aArray.length > 5){
+            console.log("too long")
+            display = "ERR";
+            showDisplay.innerHTML = display;
+    
+        } else if(aArray.length <= 5) {
+
+            a = aArray.join("");
+            display = a;
+            showDisplay.innerHTML = display;
+
+        }
+
+    } else {
+
+        /* untested
+        bArray.push(7);
+        display = bArray.join("");
+        */
+    }
 })
 
 const eightButton = document.querySelector("#eight");
 eightButton.addEventListener(`click`, (e) => {
-    console.log("eight button pressed");
-    display = 8;
-    showDisplay.innerHTML = display;
+
+    if (param == null){
+
+        aArray.push(8);
+        a = aArray.join("");
+
+        if (aArray.length > 5){
+            console.log("too long")
+            display = "ERR";
+            showDisplay.innerHTML = display;
+    
+        } else if(aArray.length <= 5) {
+
+            a = aArray.join("");
+            display = a;
+            showDisplay.innerHTML = display;
+
+        }
+
+    } else {
+
+        /* untested
+        bArray.push(8);
+        display = bArray.join("");
+        */
+    }
+
 })
 
 const nineButton = document.querySelector("#nine");
 nineButton.addEventListener(`click`, (e) => {
-    console.log("nine button pressed");    
-    display = 9;
-    showDisplay.innerHTML = display;
+
+    if (param == null){
+
+        aArray.push(3);
+        a = aArray.join("");
+
+        if (aArray.length > 5){
+            console.log("too long")
+            display = "ERR";
+            showDisplay.innerHTML = display;
+    
+        } else if(aArray.length <= 5) {
+
+            a = aArray.join("");
+            display = a;
+            showDisplay.innerHTML = display;
+
+        }
+
+    } else {
+
+        /* untested
+        bArray.push(3);
+        display = bArray.join("");
+        */
+    }
+
 })
 
 const zeroButton = document.querySelector("#zero");
 zeroButton.addEventListener(`click`, (e) => {
-    console.log("zero button pressed");
-    display = 0;
-    showDisplay.innerHTML = display;
+
+    if (param == null){
+
+        if (aArray.length === 0){
+            display = 0;
+            showDisplay.innerHTML = display;
+
+        } else if (aArray.length > 0){
+
+            aArray.push(0);
+            a = aArray.join("");
+            display = a;
+            showDisplay.innerHTML = display;
+
+        } else if (aArray.length > 5){
+            console.log("too long")
+            display = "ERR";
+            showDisplay.innerHTML = display;
+    
+        }  else if (aArray.length <= 5) {
+            a = aArray.join("");
+
+        }
+
+
+
+
+
+    } else {
+
+        /* untested
+        bArray.push(3);
+        display = bArray.join("");
+        */
+    }
 })
 
 const periodButton = document.querySelector("#period");
