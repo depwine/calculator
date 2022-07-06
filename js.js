ddde528b489fdcec6da340d61fcfed846f228a43
Glib Lakeev <glakeev@gmail.com>
2022-07-05 23:09:40 -0400

    hooked up basic button functionality, changed ui and css a little bit

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
+//declare display #
+let display = 0;
+
 //add
 const add = function(a, b){
     return a+b;
@@ -54,6 +57,108 @@ const operate = function (param, a, b){
     }
 
 }
+
+//oper button functionality
+const addButton = document.querySelector("#add");
+    addButton.addEventListener(`click`, function(e){
+        console.log("add button pressed");
+    })
+
+const subtractButton = document.querySelector("#subtract");
+subtractButton.addEventListener(`click`, function(e){
+    console.log("subtract button pressed");
+})
+
+const multiplyButton = document.querySelector("#multiply");
+multiplyButton.addEventListener(`click`, function(e){
+    console.log("multiply button pressed");
+})
+
+const divideButton = document.querySelector("#divide");
+divideButton.addEventListener(`click`, function(e){
+    console.log("divide button pressed");
+})
+
+const equalButton = document.querySelector("#equal");
+equalButton.addEventListener(`click`, function(e){
+    console.log("equal button pressed");
+})
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener(`click`, function (e){
+    console.log("clear button pressed");
+})
+
+const percentButton = document.querySelector("#percent");
+percentButton.addEventListener(`click`, (e) =>{
+    console.log("percent button pressed");
+})
+
+const posnegButton = document.querySelector("#posNeg");
+posnegButton.addEventListener(`click`, (e) => {
+    console.log("pos neg button pressed");
+})
+
+//0-9, period 
+
+const oneButton = document.querySelector("#one");
+oneButton.addEventListener(`click`, (e) => {
+    console.log("one button pressed");
+})
+
+const twoButton = document.querySelector("#two");
+twoButton.addEventListener(`click`, (e) => {
+    console.log("two button pressed");
+})
+
+const threeButton = document.querySelector("#three");
+threeButton.addEventListener(`click`, (e) => {
+    console.log("three button pressed");
+})
+
+const fourButton = document.querySelector("#four");
+fourButton.addEventListener(`click`, (e) => {
+    console.log("four button pressed");
+})
+
+const fiveButton = document.querySelector("#five");
+fiveButton.addEventListener(`click`, (e) => {
+    console.log("five button pressed");
+})
+
+const sixButton = document.querySelector("#six");
+sixButton.addEventListener(`click`, (e) => {
+    console.log("six button pressed");
+})
+
+const sevenButton = document.querySelector("#seven");
+sevenButton.addEventListener(`click`, (e) => {
+    console.log("seven button pressed");
+})
+
+const eightButton = document.querySelector("#eight");
+eightButton.addEventListener(`click`, (e) => {
+    console.log("eight button pressed");
+})
+
+const nineButton = document.querySelector("#nine");
+nineButton.addEventListener(`click`, (e) => {
+    console.log("nine button pressed");    
+})
+
+const zeroButton = document.querySelector("#zero");
+zeroButton.addEventListener(`click`, (e) => {
+    console.log("zero button pressed");
+})
+
+const periodButton = document.querySelector("#period");
+periodButton.addEventListener(`click`, (e) => {
+    console.log("period button pressed");
+})
+
+
+
+
 
 /*
 //test
